2e7c98b994a539c4eaf17ca6016cfdaaabd59967
Alngzuige <2835741198@qq.com>
2020-09-10 19:30:22 +0800

    第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,67 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="header">
+      <div>
+        <img src="./static/images/home/logo@2x.png" alt="" />
+      </div>
+      <div class="nav">
+        <router-link tag="p" to="/">首页</router-link>
+        <router-link tag="p" to="/course">课程</router-link>
+        <router-link tag="p" to="/information">实训</router-link>
+        <router-link tag="p" to="/job">就业</router-link>
+        <router-link tag="p" to="/practical">资讯</router-link>
+      </div>
+      <div class="seach">
+        <div class="input">
+          <input type="text" placeholder="搜索感兴趣的课程" />
+        </div>
+        <div>
+          <p>登录</p>
+          <p>|</p>
+          <p>注册</p>
+        </div>
+      </div>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<style lang="scss" scoped>
+.header {
+  width: 1200px;
+  height: 80px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .nav {
+    width: 300px;
+    display: flex;
+    justify-content: space-between;
+  }
+  .seach {
+    width: 400px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    div {
+      display: inherit;
+      p {
+        margin: 0 3px;
+      }
+    }
+    .input {
+      width: 255px;
+      height: 30px;
+      border: 1px solid #ccc;
+      border-radius: 20px;
+      box-sizing: border-box;
+      padding: 0 20px;
+      input {
+        width: 100%;
+        border: none;
+        outline: none;
+      }
     }
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
+import Course from '../views/Course.vue'
+import Information from '../views/Information.vue'
+import Job from '../views/Job.vue'
+import Practical from '../views/Practical.vue'
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [
+  { path: '/', component: Home },
+  { path: '/course', component: Course },
+  { path: '/information', component: Information },
+  { path: '/job', component: Job },
+  { path: '/practical', component: Practical },
 ]
 
 const router = new VueRouter({
